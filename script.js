866eed8f8b0bd2382522282f58d9ec2055a08ee4
ManarAlfailkawi <110580894+meme2935@users.noreply.github.com>
2023-09-04 19:50:19 +0300

    cw5

---
+++ script.js
@@ -0,0 +1,39 @@
+
+    alert ('مرحبا بك في موقعي , أنا منار وسوف احسب درجتك')
+
+let grade = prompt("قم بادخال درجتك")
+alert(grade);
+
+console.log("%c grade", "color:green;");
+
+if (grade<= 100 && grade>=90 ){
+console.log("مبرووك !! حصلت على امتياز")
+
+}
+else if (grade<=89 && grade>=80  ){
+    console.log("أحسنت !! لقد حصلت على جيد جداَ")
+    
+    }
+
+    else if (grade<=79 && grade>=70  ){
+        console.log(" لقد حصلت على جيد ")
+        
+        }
+
+        else if (grade<=69 && grade>=60  ){
+            console.log("لقد حصلت على مقبول ")
+            
+            }
+
+            else if (grade<=59 && grade>=50  ){
+                console.log("لقد حصلت على ضعيف ")
+                
+                }
+
+                else if (grade<50  ){
+                    console.log("راسب")
+                }
+                // جافا سكربت ليست جافا
+                
+                
+
